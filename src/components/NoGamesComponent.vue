2703ce34ed9dd13afc28c154914035ad8bcc1a09
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-02-04 16:57:11 +0300

    définition du composant no-games: cet composant est affiché s'il n'y a aucun jeux dans la base de données

---
+++ src/components/NoGamesComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="no-games">
+        <h3>No <span>games</span> availables 🎮</h3>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "NoGamesComponent"
+}
+
+
+</script>
+
+<style>
+
+#no-games {
+    width: 50%;
+    height: max-content;
+}
+
+</style>
