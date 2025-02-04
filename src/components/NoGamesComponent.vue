fafbc37f290586f951304e5852698d8922751c23
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-02-04 17:18:59 +0300

    composants no games bien développé et importé dans la page d'accueil

--- src/components/NoGamesComponent.vue
+++ src/components/NoGamesComponent.vue
@@ -1,13 +1,18 @@
 <template>
-    <div id="no-games">
-        <h3>No <span>games</span> availables 🎮</h3>
+    <div id="no-games" v-if="noGames">
+        <h3>No <span class="red">games</span> availables 🎮</h3>
     </div>
 </template>
 
 <script>
 
 export default {
-    name: "NoGamesComponent"
+    name: "NoGamesComponent",
+    data(){
+        return {
+            noGames: false
+        }
+    }
 }
 
 
@@ -18,6 +23,14 @@ export default {
 #no-games {
     width: 50%;
     height: max-content;
+    margin: 100px auto 100px auto;
+    text-align: center;
+    font-size: 30px;
+    color: #333;
+}
+
+.red {
+    color: #ff0000;
 }
 
 </style>
