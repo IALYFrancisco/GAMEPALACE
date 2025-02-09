7dfc767f112b995ef3bb936c0af9b56d17b881d1
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-02-09 15:03:20 +0300

    définition de store de jeux avec des améliorations au niveau des états de requête

--- src/stores/store.js
+++ src/stores/store.js
@@ -13,3 +13,37 @@ export const userStore = createStore({
     }
   }
 })
+
+export const gameStore = createStore({
+  state(){
+    return {
+      listOfGames: [],
+      stateOfGetGamesListRequest: false
+    }
+  },
+  mutations: {
+    async getListOfGames(state){
+      try {
+        await axios({
+          method: 'GET',
+          url: `${import.meta.env}/game`
+        }).then((response) => {
+          // la liste contient au moins un élément
+          if(response.status == 200){
+            state.listOfGames = response.data
+          }
+          // la liste est vide
+          else if(response.status == 204){
+            console.log("Il n'y a aucun jeu dans la liste")
+          }
+        }).catch((error) => {
+          console.log(`Erreur de récupération de liste de jeux: ${error}`)
+        })
+      }catch(error){
+        console.log(`Erreur de récupération de liste de jeux`)
+      }finally{
+        state.stateOfGetGamesListRequest = true
+      }
+    }
+  }
+})
