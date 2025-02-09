f119e1b8d9cd56b41f08aa666ec7e5c9899cea04
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-02-09 09:04:28 +0300

    suppression du store des jeux

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script>
-import {gameStore} from '@/stores/store'
-
 export default {
   name: 'App'
 }
--- src/stores/store.js
+++ src/stores/store.js
@@ -13,33 +13,3 @@ export const userStore = createStore({
     }
   }
 })
-
-
-export const gameStore = createStore({
-    state(){
-        return {
-        }
-    },
-    getters: {
-        async getListOfGames(){
-            try {
-                await axios({
-                    method: 'GET',
-                    url: `${import.meta.env.VITE_BASE_URL}/game`
-                }).then((response) => {
-                    // la liste contient au moins un élément
-                    if(response.status == 200){
-                        return response.data
-                    }
-                    // aucun élément dans la liste des jeux
-                    else if (response.status == 204){
-                        return
-                    }
-                })
-            }catch(error){
-                console.log(`Erreur de récupération de liste des jeux: ${error}`)
-                return
-            }
-        }
-    }
-})
