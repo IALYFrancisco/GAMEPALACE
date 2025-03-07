88125142de9693efc631599beb0a1d9e32d8c79b
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-03-08 00:48:50 +0300

    création de fichier de vue pour la pag détails de jeu

---
+++ src/views/DetailsGameView.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Game id is : {{ ID }}</h1>
+</template>
+
+<script>
+
+export default {
+    name: "DetailsGameView",
+    data(){
+        return {
+            ID: this.$route.params.id
+        }
+    }
+}
+
+</script>
