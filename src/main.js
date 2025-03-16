61b3b4183f974c9248992d20eac37f5f0640d8b5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-03-16 12:10:02 +0300

    changemento du store user en store authentification

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import { userStore } from './stores/store'
+import { authenticationStore } from './stores/store'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(userStore).mount('#app')
+app.use(authenticationStore).mount('#app')
---
+++ src/stores/store.js
@@ -0,0 +1,11 @@
+import { createStore } from "vuex";
+
+export const authenticationStore = createStore({
+  state(){
+    return {
+
+    }
+  },getters:{
+
+  }
+})
